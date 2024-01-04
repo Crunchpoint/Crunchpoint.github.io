04422db52789c501d4b15f1a64eeb580f0aee734
Johnny <mycoside@gmail.com>
2024-01-04 20:12:03 +0900

    .

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "첫 포스팅",
-        "excerpt":"테스트중   테스트중  ","categories": [],
-        "tags": [],
-        "url": "/test/",
+        "title": "[Python] 환경변수 .env 사용법",
+        "excerpt":"dot-env 기본 사용 방법 개발을 하다보면 외부에서 open api키를 받아와서 코드에 직접 사용 하거나 데이터베이스 접속을 위해 id 또는 password등을 입력해야 하는 상황이 있다 이럴때 걱정 되는 점 중에 하나가, github에 업로드한 repository를 private로 따로 설정을 해두지 않았다면 모든 사람에게 민감한 정보가 그대로 유출될 수 있다는 것 이다 보편적으로 .env는...","categories": [],
+        "tags": ["python","dot-env"],
+        "url": "/test2/",
         "teaser": null
       }]
